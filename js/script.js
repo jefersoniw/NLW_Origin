6316ccc98c628e71aab15b6a470dd831d2d7228a
jeferson chagas silva <jeferson_chagas25@hotmail.com>
2021-10-24 00:56:33 -0300

    NLW | Stage 4, SUbstituindo informações colocando via js, por informações consumidas da api do github.

--- js/script.js
+++ js/script.js
@@ -14,3 +14,21 @@ function changeSocialMediaLinks() {
 }
 
 changeSocialMediaLinks()
+
+//----------------------------------------------------
+
+function getGithubProfileInfos() {
+  const url = `https://api.github.com/users/${linksSocialMedia.github}`
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      userName.textContent = data.name
+      userBio.textContent = data.bio
+      userLink.href = data.html_url
+      userPhoto.src = data.avatar_url
+      userLogin.textContent = data.login
+    })
+}
+
+getGithubProfileInfos()
